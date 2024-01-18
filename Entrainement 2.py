f57878b1549cd6ed37f6cb82f5720a4891a9e8f0
Kobainnn <156604148+Kobainnn@users.noreply.github.com>
2024-01-18 22:39:46 +0100

    Add files via upload

---
+++ Entrainement 2.py
@@ -0,0 +1,21 @@
+nombre_a_gauche = input()
+nombre_a_droite = input()
+
+operation = input()
+
+resultat = 0
+
+nombre_a_gauche.isnumeric()
+nombre_a_droite.isnumeric()
+
+if not nombre_a_gauche.isnumeric() or nombre_a_gauche.isnumeric() :
+    print("Erreur: les deux nombres doivent être des nombres entiers")
+else:
+    nombre_a_droite = int(nombre_a_droite) 
+    nombre_a_gauche = int(nombre_a_gauche)
+
+
+
+
+
+
